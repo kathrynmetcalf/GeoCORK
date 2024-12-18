6409879fefc9fe9a6d88b44e1d4de07102cf0242
jarrod-burges <46946351+jarrod-burges@users.noreply.github.com>
2024-12-18 07:16:54 -0800

    Update mainAppEngine.py

--- mainAppEngine.py
+++ mainAppEngine.py
@@ -1,14 +1,18 @@
 
 import sys
 
-from PyQt6.QtWidgets import QApplication
+from PyQt6.QtWidgets import QApplication, QErrorMessage
 
 from ui.LandingUI import LandingPage
 
 import signal
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 
+def error_handler(type, value, traceback):
+    QErrorMessage().showMessage(f"{''.join(traceback.format_exception(type, value, traceback))}")
+
 if __name__ == "__main__":
+    sys.excepthook = error_handler
     app = QApplication(sys.argv)
 
     landing_page = LandingPage()
